ebf74a44055ffa318a3832318254f7ac73dc6fd4
Brion Finlay <brion.finlay@gmail.com>
2020-07-07 01:22:27 -0500

    table fix

--- src/SpreadsheetSeederMediator.php
+++ src/SpreadsheetSeederMediator.php
@@ -99,7 +99,7 @@ class SpreadsheetSeederMediator
     }
 
     private function checkTable() {
-        $tableName = $this->sourceSheet->getTableName();
+        $tableName = isset($this->settings->tablename) ? $this->settings->tablename : $this->sourceSheet->getTableName();
         $this->seedTable = new DestinationTable($tableName, $this->settings);
 
 
---
+++ tests/TablenameTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class TablenameTest extends TestCase
+{
+    public function test_worksheet_name() {
+
+    }
+
+    public function test_csv_filename() {
+
+    }
+
+    public function test_single_sheet_workbook_name() {
+
+    }
+
+    public function test_settings_tablename_name() {
+
+    }
+}
